0cc7aa0b0b02305b0541125b7da9350a9420b168
AsifAliSoftage <153803870+AsifAliSoftage@users.noreply.github.com>
2023-12-20 11:10:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit',function(event) {
+  event.preventDefault();
+  alert( "Credentials not saved or sent.");
+});
+                                          
